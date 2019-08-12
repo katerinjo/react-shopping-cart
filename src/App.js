4ca3591ba9691fc5f655aa351add78d4aa4b2482
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-08-12 14:21:08 -0700

    update Renders

--- src/App.js
+++ src/App.js
@@ -23,14 +23,12 @@ function App() {
 		<ProductContext.Provider value={{ products, addItem }}>
 			<CartContext value={cart}>
 				<div className="App">
-					<Navigation cart={cart} />
+					<Navigation />
 
 					{/* Routes */}
 					<Route exact path="/" component={Products} />
 
-					<Route
-						path="/cart"
-						render={() => <ShoppingCart cart={cart} />}
+					<Route path="/cart" component={ShoppingCart} />}
 					/>
 				</div>
 			</CartContext>
